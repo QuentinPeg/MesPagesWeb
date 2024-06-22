e5a30a6e1befe45ee9522d31b9a65b924b6feeee
peguinq <quentin.peguin@etu.univ-grenoble-alpes.fr>
2024-06-22 16:23:28 +0200

    mise a jour du footer

--- Site_Manon/V16/php/footer.php
+++ Site_Manon/V16/php/footer.php
@@ -48,20 +48,26 @@
 </script>
 </main>
 <footer>
-    <p>Une petite note sur moi : ...</p>
-    <h2>Merci de votre visite​​ ! A bientôt​​ !</h2>
-    <div>
-        <a href="../php/conditions&mentions.php">
-            <p>Coder par Peguin Quentin</p>
-        </a>
-        <a href="../php/conditions&mentions.php">
-            <p>&copy; Tout droit d'auteur réservé</p>
-        </a>
-        <a href="../php/conditions&mentions.php">
-            <p>Conditions d'utilisation & Mentions légales</p>
-        </a>
-    </div>
-</footer>
+        <p>Une petite note sur moi :
+            J'ai été étudiante infirmière, c'est à ce moment-là que ces fiches ont été créées. La base de ces fiches
+            s'est construit sur les apports théoriques vus en cours et en stage ainsi les apports que j'ai pu voir et
+            apprendre
+            en stage.
+            Ces fiches m'ont permis d'étudier et de réviser mes partiels pendant mes trois années de formation,
+            et m'ont conduite jusqu'à l'obtention de mon diplôme. Une fois diplômée j'ai travaillé en service d'urgence
+            puis en hospitalisation à domicile.</p>
+        <h2>Merci de votre visite​​ ! A bientôt​​ !</h2>
+        <div>
+            <a href="../php/conditions&mentions.php">
+                <p>Coder par Peguin Quentin</p>
+            </a>
+            <a href="../php/conditions&mentions.php">
+                <p>&copy; Tout droit d'auteur réservé</p>
+            </a>
+            <a href="../php/conditions&mentions.php">
+                <p>Conditions d'utilisation & Mentions légales</p>
+            </a>
+    </footer>
 <script src="../js/precommande.js"></script>
 </body>
 
