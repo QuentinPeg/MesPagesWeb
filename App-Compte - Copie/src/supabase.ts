8fce880dcefef61be80cbf109f97500f678ac612
peguinq <quentin.peguin@etu.univ-grenoble-alpes.fr>
2024-10-23 08:29:51 +0200

    suppression de App compte copie (ATTENTION DOUTE SUR VERSION LAPLUS ECENTE ENTRE APP COMPTE ET APP COMPTE COPIE)

--- App-Compte - Copie/src/supabase.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://kbkzeihbxlpsvkjpczms.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtia3plaWhieGxwc3ZranBjem1zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjExOTk2NzQsImV4cCI6MjAzNjc3NTY3NH0.fRsuMrxSGlAYFNGhysj46ajeecKWf5IGaJtchtMQA2Y';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
-
