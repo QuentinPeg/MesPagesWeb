cb617b0c1818549b739c5d97e1605514636191f3
peguinq <quentin.peguin@etu.univ-grenoble-alpes.fr>
2024-11-26 08:56:26 +0100

    suppression testsiteeract

--- testsitereact/src/components/components.ts
+++
@@ -1,6 +0,0 @@
-export {default as ResultatLOL} from './ResultatLOL';
-export {default as Footer} from './Footer';
-export {default as ImageSection} from './ImageSection';
-export {default as Navbar} from './Navbar';
-export {default as Newsletter} from './Newsletter';
-export {default as PricingCard} from './PricingCard';
