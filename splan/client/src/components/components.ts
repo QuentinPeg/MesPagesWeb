e6c5293e5190294f0c055a3942ca0684372f963b
Quentin Peguin <152846867+QuentinPeg@users.noreply.github.com>
2024-11-26 08:27:24 +0100

    Delete splan directory

--- splan/client/src/components/components.ts
+++
@@ -1,7 +0,0 @@
-export {default as Banner} from './Banner';
-export {default as Footer} from './Footer';
-export {default as ImageSection} from './ImageSection';
-export {default as Navbar} from './Navbar';
-export {default as Newsletter} from './Newsletter';
-export {default as PricingCard} from './PricingCard';
-export {default as Post} from './Post';
--- splan/client/supabaseClient.ts
+++
@@ -1,9 +0,0 @@
-// Supabase client
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://lgxpumurghtoclmioznf.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxneHB1bXVyZ2h0b2NsbWlvem5mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ1ODYzNDQsImV4cCI6MjAzMDE2MjM0NH0.h0sb8bk68uvO3EA36DVNMSYAVPuwo9ccUnTu9MweJiw'
-
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export { supabase };
